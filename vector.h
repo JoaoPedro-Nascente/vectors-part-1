b353bcc41e6860e3fc3c50e2388364e5d48b647d
JoaoPedro-Nascente <nascentejp@hotmail.com>
2024-03-27 21:53:03 -0300

    debug

--- vector.h
+++ vector.h
@@ -43,4 +43,34 @@ int vector_argmax(Vector *v);
 // Retorna o índice do menor elemento do vector (assumindo que podemos comparar elementos usando o operador "<")
 int vector_argmin(Vector *v);
 
+// Remove o i-ésimo elemento do vetor.
+data_type vector_remove(Vector *v, int i);
+
+// Remove o primeiro elemento
+data_type vector_pop_front(Vector *v);
+
+// Remove o ultimo elemento
+data_type vector_pop_back(Vector *v);
+
+// Insere o elemento na i-esima posicao
+void vector_insert(Vector *v, int i, data_type val);
+
+// Troca os elementos das posições i e j (i vira j e j vira i)
+void vector_swap(Vector *v, int i, int j);
+
+// Ordena o vetor in-place (sem criar um novo vetor)
+void vector_sort(Vector *v);
+
+// Retorna o indice de val usando busca binaria. Retorna -1 se nao encontrado.
+int vector_binary_search(Vector *v, data_type val);
+
+// Inverte o vetor in-place (sem criar um novo vetor)
+void vector_reverse(Vector *v);
+
+// Cria uma cópia do vector e dos valores de seus atributos.
+Vector *vector_copy(Vector *v);
+
+// Remove todos os elementos de v
+void vector_clear(Vector *v);
+
 #endif
